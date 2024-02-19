4bb3dedaf2edf716e03a000fb2011a5d2716f279
nayon117 <hasibul.nayon1@gmail.com>
2024-02-19 15:06:31 +0600

    create signup page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,11 +1,11 @@
-// STARTER CODE FOR THIS FILE
+
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
         <h1 className="text-3xl font-semibold text-center text-gray-300">
           Login
-          <span className="text-blue-500"> ChatApp</span>
+          <span className="text-blue-500"> ChitChat</span>
         </h1>
 
         <form>
@@ -35,6 +35,7 @@ const Login = () => {
             className="text-sm  hover:underline hover:text-blue-600 mt-2 inline-block"
           >
             {"Don't"} have an account?
+           <span>Signup</span> 
           </a>
 
           <div>
