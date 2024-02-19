a6187dbe30a82bdafec76b4894b9aa899d398627
nayon117 <hasibul.nayon1@gmail.com>
2024-02-19 16:39:28 +0600

    create logoutbutton component

---
+++ src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,9 @@
+import { IoLogOut } from "react-icons/io5";
+
+const LogoutButton = () => {
+    return(
+        <div className="mt-auto">
+             <IoLogOut className="text-2xl cursor-pointer text-white"/>
+        </div>
+    )}
+export default LogoutButton;
