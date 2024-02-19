b02f0867c5c23cd3b0deacfff39e7ffdb1309eb9
nayon117 <hasibul.nayon1@gmail.com>
2024-02-19 16:11:47 +0600

    create search Input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
 
 function App() {
   return (
     <div className="p-4 flex justify-center items-center h-screen">
-      <Login />
+      {/* <Login /> */}
+      <SignUp/>
     </div>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,9 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
     return(
-        <div>
-             <p> This is Home </p>
+        <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0">
+             <Sidebar/>
         </div>
     )}
 export default Home;
