4e9ff48f905e21e5307ddc63ab5ab99b8997170c
nayon117 <hasibul.nayon1@gmail.com>
2024-02-21 12:53:48 +0600

    show dynamic messages with time

--- src/components/messages/Message.jsx
+++ src/components/messages/Message.jsx
@@ -1,20 +1,33 @@
-const Message = () => {
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
+
+const Message = ({message}) => {
+
+  const {authUser} = useAuthContext()
+  const {selectedConversation} = useConversation()
+
+  const fromMe = message.senderId === authUser._id;
+
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe ? authUser?.profilePic : selectedConversation?.profilePic
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "" 
+
   return (
-    <div className="chat chat-end  ">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar ">
         <div className="w-10 h-10 rounded-full">
           <img
             className="w-10"
             alt="Tailwind CSS chat bubble component"
-            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            src={profilePic}
           />
         </div>
       </div>
-      <div className="chat-bubble bg-blue-500 text-white">
-        Hey how are you
+      <div className={`chat-bubble   ${bubbleBgColor}`}>
+       {message.message}
       </div>
-      <div className={`chat-footer text-xs opacity-50 flex gap-1 items-center`}>
-        fine
+      <div className={`chat-footer text-xs opacity-50 text-white flex gap-1 items-center`}>
+        {message.createdAt}
       </div>
     </div>
   );
--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -1,22 +1,24 @@
 import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";
 
 const Messages = () => {
-    const {loading, messages} = useGetMessages();
-    console.log("messagesss", messages);
-    return(
-        <div className="overflow-auto px-4 flex-1">
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        <Message/>
-        </div>
-    )}
+  const { loading, messages } = useGetMessages();
+  console.log("messagesss", messages);
+  return (
+    <div className="overflow-auto px-4 flex-1">
+
+
+        {!loading && messages.length>0 && messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
+
+
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-white">Send a message to start the conversation</p>
+      )}
+    </div>
+  );
+};
 export default Messages;
