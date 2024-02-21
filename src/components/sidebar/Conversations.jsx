8c42a15a2a52ceb31da12a8a92b37c86396d8516
nayon117 <hasibul.nayon1@gmail.com>
2024-02-21 10:53:49 +0600

    create getConversation hook to get all the user

--- src/components/sidebar/Conversations.jsx
+++ src/components/sidebar/Conversations.jsx
@@ -1,14 +1,19 @@
+import useGetConversations from "../../hooks/useGetConversations";
 import Conversation from "./Conversation";
 
 const Conversations = () => {
-    return(
-        <div className="py-2 flex flex-col overflow-auto">
-              <Conversation/>
-              <Conversation/>
-              <Conversation/>
-              <Conversation/>
-              <Conversation/>
-              <Conversation/>
-        </div>
-    )}
+  const { loading, conversations } = useGetConversations();
+  console.log("CONVERSATION", conversations);
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+
+        {conversations.map((conversation)=>(
+            <Conversation key={conversation.id} conversation={conversation} />
+        ))}
+
+
+      {loading ? <span className="loading loading-spinner"></span> : null}
+    </div>
+  );
+};
 export default Conversations;
