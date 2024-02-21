6045fa58f17876437c3c4be83c998490590d60f1
nayon117 <hasibul.nayon1@gmail.com>
2024-02-21 12:24:33 +0600

    create message skeleton component

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -1,6 +1,9 @@
+import useGetMessages from "../../hooks/useGetMessages";
 import Message from "./Message";
 
 const Messages = () => {
+    const {loading, messages} = useGetMessages();
+    console.log("messagesss", messages);
     return(
         <div className="overflow-auto px-4 flex-1">
         <Message/>
